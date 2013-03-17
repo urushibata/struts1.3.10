961eab6ec6556c74066517b69279ffb109ba1f97
urushibata <urush1bata.lala@gmail.com>
2013-03-18 00:31:02 +0900

    first commit

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/7f/0070b7f4708e001219b3d2f4fe671c14
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author urush_000
+ *
+ */
+package com.urushibata.struts1.action;
---
+++ struts1.3.10/src/com/urushibata/struts1/controller/front/FrontController.java
@@ -0,0 +1,20 @@
+package com.urushibata.struts1.controller.front;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+/*
+ * フロントコントローラー
+ */
+public class FrontController extends Action{
+
+	public ActionForward execute(ActionMapping mapping, ActionForm form,
+													HttpServletRequest req, HttpServletResponse res) {
+		return mapping.findForward("success");
+	}
+}
