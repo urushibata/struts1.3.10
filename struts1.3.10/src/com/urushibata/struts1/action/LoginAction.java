3c17d2366819d8262c61940de894533cde4c451b
urushibata <urush1bata.lala@gmail.com>
2013-03-20 01:23:23 +0900

    20130319

--- struts1.3.10/src/com/urushibata/struts1/action/LoginAction.java
+++ struts1.3.10/src/com/urushibata/struts1/action/LoginAction.java
@@ -8,16 +8,25 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
+import org.apache.struts.action.ActionMessage;
+import org.apache.struts.action.ActionMessages;
 import org.apache.struts.actions.*;
+import org.apache.struts.action.ActionMessage;
+import org.apache.struts.action.ActionMessages;
 
 import com.urushibata.struts1.businessLogic.UserAuthentication;
+import com.urushibata.struts1.dto.LoginDTO;
+import com.urushibata.struts1.form.LoginFormBean;
 
 public class LoginAction extends MappingDispatchAction{
+	private LoginFormBean lf;
+	public LoginDTO dto = new LoginDTO();
 	/*
 	 * デフォルト
 	 */
-	public ActionForward execute(ActionMapping mapping, ActionForm form,
+	public ActionForward init(ActionMapping mapping, ActionForm form,
 													HttpServletRequest req, HttpServletResponse res) {
+		System.out.println("login action init !!");
 		return mapping.findForward("success");
 	}
 
@@ -29,7 +38,29 @@ public class LoginAction extends MappingDispatchAction{
 		/*
 		 * 認証ビジネスロジック
 		 */
-		UserAuthentication ut = new UserAuthentication();
-		return mapping.findForward(ut.authentication());
+		System.out.println("login action authentication !!");
+		String result ="";
+		ActionMessages ams = new ActionMessages();
+		ActionMessage msg;
+
+		lf = (LoginFormBean)form;
+		copyActionForm(lf);
+		UserAuthentication ut = new UserAuthentication(dto);
+		result = ut.authentication();
+		if(result == "failure"){
+			msg = new ActionMessage("ＩＤまたはパスワードが違います。");
+			ams.add(ActionMessages.GLOBAL_MESSAGE, msg);
+			super.saveErrors(req, ams);
+		}
+
+		return mapping.findForward(result);
+	}
+
+	/*
+	 * ActionFormをDTOにセット
+	 */
+	private void copyActionForm(LoginFormBean lf){
+		this.dto.setUserId(lf.getUserId());
+		this.dto.setUserPassword(lf.getUserPassword());
 	}
 }
--- struts1.3.10/src/com/urushibata/struts1/businessLogic/UserAuthentication.java
+++ struts1.3.10/src/com/urushibata/struts1/businessLogic/UserAuthentication.java
@@ -4,29 +4,44 @@ import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
-
-import java.sql.Connection;
 import java.sql.Statement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Connection;
+import org.apache.struts.action.ActionMessage;
+import org.apache.struts.action.ActionMessages;
+
+import com.urushibata.struts1.dto.LoginDTO;
 
 /*
  * ユーザ認証クラス
  */
 public class UserAuthentication {
+	private LoginDTO dto;
 	/*
 	 * コンストラクタ
 	 */
 	public UserAuthentication(){}
+	public UserAuthentication(LoginDTO argdto){
+		dto = argdto;
+	}
 
 	/*
 	 * ユーザ認証
 	 */
 	public String authentication() throws SQLException{
+		System.out.println("authentication businessLogig");
+
 		Connection conn = null;
 		Statement stmt = null;
+		ResultSet rest = null;
 		String result = "";
-		String query = "select * from users where userId ='' and userpassword = '';";
+		ActionMessages ams = new ActionMessages();
+		ActionMessage msg;
+		String userId = dto.getUserId();
+		String userPassword = dto.getUserPassword();
+		String query = "select * from users where userId ='" + userId + "' and userpassword = '" + userPassword + "'";
+		System.out.println("execute SQL Query :" + query);
 
 		try{
 			Context initContext = new InitialContext();
@@ -35,13 +50,19 @@ public class UserAuthentication {
 			conn = ds.getConnection();
 
 			stmt = conn.createStatement();
-			ResultSet rest = stmt.executeQuery(query);
+			rest = stmt.executeQuery(query);
 
 		}catch(NamingException ex){
-			result = "faild";
+			result = "failure";
 		}
-		//result = "success";
-		result = "failure";
+
+		if(rest.next()){
+			msg = new ActionMessage("ようこそ！");
+			ams.add(ActionMessages.GLOBAL_MESSAGE, msg);
+			result = "success";
+		}else{
+			result = "failure";
+		}
 
 		return result;
 	}
